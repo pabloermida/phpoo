1ea9869d35175326a5ad289f891c917856dd8a98
pabloermida <pablo.ermida@gmail.com>
2015-06-22 19:20:56 -0300

    Correção exercício 1

--- exercicio1.php
+++ exercicio1.php
@@ -23,12 +23,16 @@ for ($i=0; $i<10; $i++)
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <style>
+
+    </style>
 </head>
 
 <body>
@@ -36,47 +40,48 @@ for ($i=0; $i<10; $i++)
 <div class="container">
     <h1>Cadastro de Clientes</h1>
     <p class="lead">Fase 1 do projeto</p>
-    <table class="table" >
+    <table id="myTable" class="table tablesorter" >
         <thead>
         <tr>
             <th>#</th>
             <th>Nome</th>
             <th>CPF</th>
         </tr>
+        </thead>
         <?php
         foreach ($cliente as $key=>$value)
         {
-        ?>
+            ?>
             <tr><td><?=$key?></td><td><a href="#" data-toggle="modal" data-target="#clienteModal<?=$key?>"><?=$value->getNome();?></a></td><td><?=$value->getCPF();?></td></tr>
         <?php
         }
         ?>
-        </thead>
+
     </table>
 </div><!-- /.container -->
 <?php
 foreach ($cliente as $key=>$value)
 {
-?>
-<div id="clienteModal<?=$key?>" class="modal fade">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title">Informações do Cliente</h4>
-            </div>
-            <div class="modal-body">
-                <p>Nome: <?=$value->getNome();?></p>
-                <p>CPF: <?=$value->getCPF();?></p>
-                <p>Endereço: <?=$value->getEndereco();?></p>
-                <p>Telefone: <?=$value->getTelefone();?></p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+    ?>
+    <div id="clienteModal<?=$key?>" class="modal fade">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title">Informações do Cliente</h4>
+                </div>
+                <div class="modal-body">
+                    <p>Nome: <?=$value->getNome();?></p>
+                    <p>CPF: <?=$value->getCPF();?></p>
+                    <p>Endereço: <?=$value->getEndereco();?></p>
+                    <p>Telefone: <?=$value->getTelefone();?></p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+                </div>
             </div>
         </div>
     </div>
-</div>
 <?php
 }
 ?>
@@ -85,11 +90,10 @@ foreach ($cliente as $key=>$value)
 <!-- Placed at the end of the document so the pages load faster -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
-
+<script type="text/javascript" src="js/jquery.tablesorter.js"></script>
 <script>
     $( document ).ready(function() {
-
-
+        $("#myTable").tablesorter();
     });
 
 </script>
